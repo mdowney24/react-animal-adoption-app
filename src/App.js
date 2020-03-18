6c9cfcce45d093014066980c9ab9c0ded0fa1b85
mdowney24 <mdowney17@qub.ac.uk>
2020-03-18 17:31:59 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+
+// stamp analogy
+const App = () => {
+  return (
+    <React.StrictMode>
+      <div>
+        <header>
+        <Link to='/'>Adopt Me!</Link>
+        </header>
+        
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
